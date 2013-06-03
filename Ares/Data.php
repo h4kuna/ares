7ea4fc1be6424b71092c7d890133b28fe6528d20
Milan Matejcek <milan.matejcek@gmail.com>
2013-06-03 09:40:05 +0200

    change autoload

---
+++ Ares/Data.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace h4kuna\Ares;
+
+use Nette\Object;
+
+/**
+ * Description of Data
+ *
+ * @author milan
+ */
+class Data extends Object implements \ArrayAccess, \Iterator {
+
+    private $data = array();
+
+    public function setIN($s) {
+        return $this->set('in', $s);
+    }
+
+    public function setTIN($s) {
+        $this->set('tin', $s);
+        return $this->set('vat_pay', (bool) $this->data['tin']);
+    }
+
+    public function setCompany($s) {
+        return $this->set('company', $s);
+    }
+
+    public function setStreet($s) {
+        return $this->set('street', $s);
+    }
+
+    public function setCity($s) {
+        return $this->set('city', $s);
+    }
+
+    public function setZip($s) {
+        return $this->set('zip', $s);
+    }
+
+    public function setActive($s) {
+        $this->data['active'] = strval($s) == 'Aktivní';
+        return $this;
+    }
+
+    public function setFileNumber($s) {
+        return $this->set('file_number', $s);
+    }
+
+    public function setCourt($s) {
+        return $this->set('court', $s);
+    }
+
+    private function setFileNumberAndCourt() {
+        if (array_key_exists('file_number', $this->data) && array_key_exists('court', $this->data)) {
+            $this->data['court_all'] = $this->data['file_number'] . ', ' . $this->data['court'];
+        }
+    }
+
+    private function set($key, $val) {
+        $this->data[$key] = strval($val);
+        return $this;
+    }
+
+    public function clean() {
+        $this->data = array();
+    }
+
+    /**
+     * copy data
+     * @param array $map
+     * @return array
+     */
+    public function toArray(array $map = array()) {
+        $this->setFileNumberAndCourt();
+        if (!$map) {
+            return $this->data;
+        }
+        $out = array();
+        foreach ($map as $k => $v) {
+            if ($this->offsetExists($k)) {
+                if (!$v) {
+                    $v = $k;
+                }
+                $out[$v] = $this->data[$k];
+            }
+        }
+        return $out;
+    }
+
+    public function __toString() {
+        return json_encode($this->data);
+    }
+
+// ---------------- ArrayAccess
+    public function offsetExists($offset) {
+        return array_key_exists($offset, $this->data);
+    }
+
+    public function offsetGet($offset) {
+        if ($this->offsetExists($offset)) {
+            return $this->data[$offset];
+        }
+        throw new \h4kuna\AresException('Undefined offset: ' . $offset);
+    }
+
+    public function offsetSet($offset, $value) {
+        return $this->data[$offset] = $value;
+    }
+
+    public function offsetUnset($offset) {
+        unset($this->data[$offset]);
+    }
+
+// ---------------- Iterator
+    public function current() {
+        return current($this->data);
+    }
+
+    public function key() {
+        return key($this->data);
+    }
+
+    public function next() {
+        return next($this->data);
+    }
+
+    public function rewind() {
+        reset($this->data);
+    }
+
+    public function valid() {
+        return array_key_exists($this->key(), $this->data);
+    }
+
+}
+
---
+++ Ares/Get.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace h4kuna\Ares;
+
+use Nette\Object;
+
+/**
+ * Description of Get
+ *
+ * @author Milan Matějček
+ */
+class Get extends Object implements IRequest {
+
+    const URL = 'http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=';
+
+    /** @var Data */
+    protected $data;
+    protected $IN;
+
+    public function __construct(Data $data = NULL) {
+        $this->data = $data ? $data : new Data;
+    }
+
+    public function loadData($inn = NULL) {
+        if ($inn === NULL) {
+            return $this->data;
+        }
+
+        return $this->loadXML($inn);
+    }
+
+    private function loadXML($inn) {
+        $this->setIN($inn);
+        $curl = new \h4kuna\CUrl(self::URL . $this->IN);
+        $xmlSource = $curl->exec();
+        if ($xmlSource) {
+            $xml = @simplexml_load_string($xmlSource);
+            if (!$xml) {
+                throw new \h4kuna\AresException('No response.', 404);
+            }
+        } else {
+            $curl->getErrors();
+        }
+
+        $ns = $xml->getDocNamespaces();
+        $el = $xml->children($ns['are'])->children($ns['D'])->VBAS;
+
+        if (!isset($el->ICO)) {
+            return $this->data;
+        }
+
+        $street = strval($el->AD->UC);
+        if (is_numeric($street)) {
+            $street = $el->AA->NCO . ' ' . $street;
+        }
+
+        if (isset($el->AA->CO)) {
+            $street .= '/' . $el->AA->CO;
+        }
+
+        $this->data->setIN($el->ICO)
+                ->setTIN($el->DIC)
+                ->setCity($el->AA->N)
+                ->setCompany($el->OF)
+                ->setStreet($street)
+                ->setZip($el->AA->PSC);
+
+        if (isset($el->ROR)) {
+            $this->data->setActive($el->ROR->SOR->SSU)
+                    ->setFileNumber($el->ROR->SZ->OV)
+                    ->setCourt($el->ROR->SZ->SD->T);
+        }
+
+        return $this->data;
+    }
+
+    private function setIN($inn) {
+        $this->IN = new \h4kuna\Int($inn);
+        if (!preg_match('~^\d{6,9}$~', $this->IN->getValue())) {
+            throw new \h4kuna\AresException('IN must be a number');
+        }
+        return TRUE;
+    }
+
+    public function clean() {
+        $this->data->clean();
+        return $this;
+    }
+
+}
+
