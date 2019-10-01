5a203dde753fb9ab04d5988c1d3b3f45039d4c3c
BigOHenry <o.sochurek@gmail.com>
2019-10-01 10:27:00 +0200

    Update Ares.php
    dissolved fix

--- src/Ares.php
+++ src/Ares.php
@@ -83,9 +83,12 @@ class Ares
 			->setCityDistrict(self::exists($xml->AA, 'NCO'))
 			->setIsPerson(self::exists($xml->PF, 'KPF'))
 			->setCreated((string) $xml->DV)
-			->setDissolved((string) $xml->DZ)
 			->setNace(self::existsArray($xml->Nace, 'NACE'))
 			->setActive(!isset($xml->DZ));
+		
+		if (isset($xml->DZ)) {
+		    	$dataProvider->setDissolved((string) $xml->DZ);
+		}
 
 		if (isset($xml->ROR)) {
 			$dataProvider
