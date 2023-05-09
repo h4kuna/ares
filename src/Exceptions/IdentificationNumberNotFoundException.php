1621c7a19dad446e74a2aef8d00598deb3a718b9
Milan Matějček <milan.matejcek@gmail.com>
2023-05-09 10:29:16 +0200

    Ares: better error info about company

--- src/Exceptions/IdentificationNumberNotFoundException.php
+++ src/Exceptions/IdentificationNumberNotFoundException.php
@@ -2,6 +2,7 @@
 
 namespace h4kuna\Ares\Exceptions;
 
+use Nette\Utils\Strings;
 use Throwable;
 
 final class IdentificationNumberNotFoundException extends AresException
@@ -12,7 +13,9 @@ final class IdentificationNumberNotFoundException extends AresException
 
 	public function __construct(string $message = '', string $in = '', Throwable $previous = null)
 	{
-		parent::__construct($message, 0, $previous);
+        $match = Strings::match($message, '/Chyba +(?<code>\d+)/');
+
+		parent::__construct($message, intval($match['code'] ?? 0), $previous);
 		$this->in = $in;
 	}
 
