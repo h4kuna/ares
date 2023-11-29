25301a1827eb316194787ac8cef810d13850e34b
Milan Matějček <milan.matejcek@gmail.com>
2023-11-29 22:58:41 +0100

    test(Json): add check for remote json

---
+++ tests/src/E2E/Ares/JsonCompareTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace h4kuna\Ares\Tests\E2E\Ares;
+
+use h4kuna\Ares\Tests\TestCase;
+use Nette\Utils\Json;
+use Tester\Assert;
+
+require __DIR__ . '/../../../bootstrap.php';
+
+final class JsonCompareTest extends TestCase
+{
+	public function testCompareJson(): void
+	{
+		$remote = Json::decode(file_get_contents('https://www.mfcr.cz/assets/attachments/2023-08-01_AresRestApi-verejne_v03.json'));
+		$local = Json::decode(file_get_contents(__DIR__ . '/../../../AresRestApi-verejne.json'));
+		Assert::equal($local, $remote);
+	}
+}
+
+(new JsonCompareTest())->run();
