f5828375c27e1212fe086d40c967f96a7a463236
Milan Matějček <milan.matejcek@gmail.com>
2024-02-22 09:49:36 +0100

    fix(Test): download json is forbidden 403

--- tests/src/E2E/Ares/JsonCompareTest.php
+++ tests/src/E2E/Ares/JsonCompareTest.php
@@ -5,6 +5,7 @@ namespace h4kuna\Ares\Tests\E2E\Ares;
 use h4kuna\Ares\Tests\TestCase;
 use Nette\Utils\Json;
 use Tester\Assert;
+use Tester\Environment;
 
 require __DIR__ . '/../../../bootstrap.php';
 
@@ -12,6 +13,10 @@ final class JsonCompareTest extends TestCase
 {
 	public function testCompareJson(): void
 	{
+		if (getenv('GITHUB_RUN') === '1') {
+			Environment::skip('Forbidden for github.');
+		}
+
 		$remote = Json::decode((string) file_get_contents('https://www.mfcr.cz/assets/attachments/2023-08-01_AresRestApi-verejne_v04.json'));
 		$local = Json::decode((string) file_get_contents(__DIR__ . '/../../../AresRestApi-verejne.json'));
 		Assert::equal($local, $remote);
