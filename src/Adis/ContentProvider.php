1ede8ac454b06cf885410ac7abe059f8b94bc1c7
Milan Matějček <milan.matejcek@gmail.com>
2024-09-30 19:17:01 +0200

    fix(tests)

--- src/Adis/ContentProvider.php
+++ src/Adis/ContentProvider.php
@@ -32,7 +32,7 @@ final class ContentProvider
 	 */
 	public function statusBusinessSubjects(array $tin): Generator
 	{
-		$duplicity = Batch::checkDuplicities($tin, fn (string $tin) => Helper::normalizeTIN($tin));
+		$duplicity = Batch::checkDuplicities($tin, static fn (string $tin) => Helper::normalizeTIN($tin));
 		$chunks = Batch::chunk($duplicity, 100);
 
 		foreach ($chunks as $chunk) {
